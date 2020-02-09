6eb264490ea016e637d10c76c0a2c93d2982aa9c
Hasan Mahmud <hasan.mahmud8177@gmail.com>
2020-02-09 17:57:22 +0600

    image upload in django

---
+++ image_or_file_upload.py
@@ -0,0 +1,56 @@
+# Step-01: settings.py
+
+#media files uploaded by user
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Step-02: urls.py
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+	...
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# Step-03: views.py
+
+if request.FILES['upload_image']:
+    upload_image = request.FILES['upload_image']
+    fs = FileSystemStorage()
+    image_name = fs.save(upload_image.name, company_image)
+    # uploaded_file_url = fs.url(image_name)
+    user.image = image_name # save into DB
+else:
+    pass
+
+
+# Step-04: home.html
+
+<form id="cityArea" action="{% url '' %}" method="POST" enctype="multipart/form-data">
+<div class="row">
+    <div class="col-sm-6">
+        <div class="form-group">
+            <label for="image" class="control-label">Image</label>
+            <input type="file" name="upload_image" class="form-control" id="imgInp"
+                   accept=".jpeg,.png,.jpg,.gif,.svg">
+            <img style="border-radius: 50%;" id="blah" src="" alt="Upload Image"
+                 height="80px" width="80px"/>
+        </div>
+    </div>
+</div>
+</form>
+
+<script>
+    function readURL(input) {
+        if (input.files && input.files[0]) { var reader = new FileReader();
+            reader.onload = function (e) { $('#blah').attr('src', e.target.result); }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#imgInp").change(function(){ readURL(this); });
+</script>
